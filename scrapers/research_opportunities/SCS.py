081f702c0b65ad1ff723b592d14f20210de358ac
Jennifer Zhu <jzhucs128@gmail.com>
2025-09-20 17:59:16 -0400

    Working on SCS csv

--- scrapers/research_opportunities/SCS.py
+++ scrapers/research_opportunities/SCS.py
@@ -1,1 +1,7 @@
+import csv
+
 # Goal: parse json
+with open('scrapers/research_opportunities/example_json/Simple Undergrad.csv', mode ='r') as file:
+    csvFile = csv.DictReader(file)
+    for lines in csvFile:
+        print(lines["Date Added"])
