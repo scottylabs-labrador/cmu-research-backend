964b87d8b7d98305e56d370d73fdc214c555ad49
Isaac <isaac.sun9000@gmail.com>
2025-09-27 19:01:08 -0400

    Updates database with parsed csv from SCS dep

--- scrapers/research_opportunities/SCS.py
+++ scrapers/research_opportunities/SCS.py
@@ -1,4 +1,6 @@
 import csv
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 CONTACT_IDENTIFIER = "Project Contact:"
 DESCRIPTION_IDENTIFIER = "Project Description:"
@@ -6,9 +8,19 @@ PREREQ_IDENTIFIER = "Prerequisite Knowledge:"
 RELEVANT_IDENTIFIER = "Link(s) to Relevant Papers:"
 URLS_IDENTIFIER = "Project URLs:"
 
+uri = "mongodb+srv://MongoAccess:<mongodbpw>@cluster0.a6odq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'), tlsAllowInvalidCertificates=True)
+db = client["CMUResearchDatabase"]
+col = db["ResearchOpportunities"]
+
+# Data (to be uploaded)
+scs_data = []
+
 # Goal: parse json
 
-with open('example_json/Simple Undergrad.csv', mode ='r') as file:
+with open('example_json/Undergraduate Research Opportunities.csv', mode ='r') as file:
     csvFile = csv.DictReader(file)
     for line in csvFile:
         info = line[""]
@@ -28,14 +40,18 @@ with open('example_json/Simple Undergrad.csv', mode ='r') as file:
         project_title = line["Project Title"]
         project_department = line["Department"]
         project_time_commitment = line["Time Commitment (Hours/Week)"]
+        
+        temp_dict = {
+	    "Project Title": project_title,
+            "Contact": project_contact,
+            "Description": project_desc,
+            "Prereqs": project_prereq,
+            "Time Commitment": project_time_commitment,
+            "Date Published": project_date_added,
+            "Relevant Links": project_relevant + project_URLs,
+            "Department": project_department
+        }
+        scs_data.append(temp_dict)
 
-        print(project_contact)
-        print(project_desc)
-        print(project_prereq)
-        print(project_relevant)
-        print(project_URLs)
-
-        print(project_date_added)
-        print(project_title)
-        print(project_department)
-        print(project_time_commitment)
+return_val = col.insert_many(scs_data)
+print(return_val)
