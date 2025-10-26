7651dece393a804c5c207e64e0b47e5dae50bb48
Brady Gho <ghobrady@gmail.com>
2025-10-26 16:31:18 -0400

    scrapper progress brady

--- scrapers/cmu_bme/scrapper.py
+++ scrapers/cmu_bme/scrapper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape(link):
+def scrape_engr(link):
     people = [];
 
     response = requests.get(link)
@@ -13,13 +13,42 @@ def scrape(link):
         text = tr.find_all('td')
 
         person_name = text[0].get_text(strip=True)
-        description = text[1].get_text(strip=True)
 
-        person = {"name": person_name, "description": description, "department": "biomedical engineering"}
-        people.append(person)
+        link = "https://www.cmu.edu/bme" + text[0].find_all('a')[0].get('href')
+
+        person = scrape_faulty_page(link)
+        
+        person["name"] = person_name
 
     return people;
 
-    
+def get_href()
 
-print(scrape('https://www.cmu.edu/bme/Research/research_faculty_listing.html#bio'))
+def scrape_faulty_page(link):
+    person = {"name": "", "college": "", "department": "", "bio": "", "phone number": "", "email": ""};
+
+    response = requests.get(link);
+    soup = BeautifulSoup(response.content, 'html.parser');
+
+    person["email"] = soup.select(".icon.email").get("href");
+    person["phone"] = soup.select(".icon.tel")[0].get("href");
+
+    header = soup.css.select("#sitename");
+    header_text = header.find_all("a");
+
+    person["department"] = header_text[0].get_text(strip=True);
+    person["college"] = header_text[1].get_text(strip=True);
+
+    bio_header = soup.find_all(lambda tag: tag.name == "h1" and tag.get_text() == "Bio")
+    bio = bio_header.next_sibling.get_text()
+
+    research_header = soup.find_all(lambda tag: tag.name == "h1" and tag.get_text() == "Research")
+    research = bio_header.next_sibling.get_text();
+
+    person["bio"] = bio
+    person["research"] = research
+    
+    return person;
+
+
+print(scrape_engr('https://www.cmu.edu/bme/Research/research_faculty_listing.html#bio')[0])
